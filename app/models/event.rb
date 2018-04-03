366f3ff5d3495f375fc6d96f2566a852086bf316
Your Name <you@example.com>
2018-04-03 14:12:24 -0300

    JuanmaCampos

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,25 @@
 class Event < ApplicationRecord
   belongs_to :place
   has_many :tickets
+  def initialize
+    @errors = ActiveModel::Errors.new(self)
+  end
+  validates :place, uniqueness: {scope: :start_date}
+  before_create :ensure_date
+
+
+  private
+	def ensure_date
+		if DateTime.now > start_date
+      event.errors[:name]
+    end
+
+  end
+
+
+
+
+
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
-  	has_many :user_tickets	
+	has_many :user_tickets
 	has_many :tickets, :through => :user_tickets
+	VALID_EMAIL = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+	validates :email, presence: true
+	validates :email, format: {with:VALID_EMAIL}
+	validates :password, presence: true
+	validates :password, length: { in: 8..12}	
+	validates :phone_change, length: { in: 9..12 }
 end
